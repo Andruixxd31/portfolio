9af8a1533fb39e656bdce3105cc038f1d4615f50
AndreyBal03 <andreybalvaneda2@gmail.com>
2024-03-25 19:37:20 -0600

    finished-component-skills

---
+++ src/components/Skill.svelte
@@ -0,0 +1,55 @@
+<script>
+  export let skill;
+  let fullGroups = Math.floor(skill.years / 5)
+  let remainingYears = skill.years % 5
+</script>
+
+<style>
+  .skill{
+    display: flex;
+    flex-direction: column; 
+    align-items: center; 
+    margin-bottom: .5em;
+  }
+
+  .skill-name {
+    margin-bottom: .5em;
+    font-size: 1.5em;
+  }
+
+  .bubble{
+    width: .7em;
+    height: .7em;
+    border-radius: 50%;
+    background-color: green;
+    margin-right: .3em;
+  }
+
+  .bubble-stack{
+    display: flex;
+    margin-bottom: .3em;
+  }
+
+  .icons{
+    width: 4em;
+  }
+
+</style>
+
+<div class="skill">
+  <img src={skill.src} class="icons" alt={skill.name}/>
+  <div class="skill-name">{skill.name}</div>
+  {#each Array(fullGroups) as _}
+    <div class="bubble-stack">
+      {#each Array(5) as _}
+        <span class="bubble"></span>
+      {/each}
+    </div>
+  {/each}
+  <div class="bubble-stack">
+    {#each Array(remainingYears) as _}
+      <span class="bubble"></span>
+    {/each}
+  </div>
+  <span>({skill.years})</span>
+</div>
---
+++ src/components/Skills.svelte
@@ -0,0 +1,32 @@
+<script>
+  import Skill from "../components/Skill.svelte";
+
+  let skills = [
+  {name: "Python", years: 5, src:"assets/langs_icons/python.svg"},
+  {name: "C#", years: 18, src:"assets/langs_icons/csharp.svg"},
+  {name: "C++", years: 5, src:"assets/langs_icons/cpp.svg"},
+  {name: "Haskell", years: 5, src:"assets/langs_icons/haskell.svg"},
+  {name: "Ruby", years: 5, src:"assets/langs_icons/ruby.svg"},
+  {name: "JS", years: 2, src:"assets/langs_icons/js.svg"},
+  {name: "React", years: 2, src:"assets/langs_icons/react.svg"},
+  {name: "MongoDB", years: 2, src:"assets/langs_icons/mongodb.svg"},
+  {name: "TypeScript", years: 2, src:"assets/langs_icons/typescript.svg"},
+  {name: "Firebase", years: 2, src:"assets/langs_icons/firebase.svg"},
+  {name: "Docker", years: 2, src:"assets/langs_icons/docker.svg"},
+  {name: "Go", years: 4, src:"assets/langs_icons/go.svg"},
+  ];
+</script>
+
+<style>
+  .Skill-table {
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    gap: 10px; 
+  }
+</style>
+
+<div class="Skill-table">
+  {#each skills as skill, indx}
+    <Skill skill={skill} key={indx}/>
+  {/each}
+</div>
